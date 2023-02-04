25d202a973ca23ff245fe8e62063f4ed4924d3eb
Siomara N Alonso Hernandez <xiomara281190@gmail.com>
2023-02-04 15:36:18 +0000

    Added repo to heroku.

--- Views/Components/header.php
+++ Views/Components/header.php
@@ -26,7 +26,7 @@ function createHeader(string $title) : void {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&family=Roboto+Mono:wght@200;300;400&display=swap" rel="stylesheet"> 
                 <link rel="icon" type="image/png" href="/public/build/media/logo/favicon.png">
-                <link rel="stylesheet" href="/public/build/css/main.css">
+                <link rel="stylesheet" href="${__DIR__}/public/build/css/main.css">
                 <title>{$title}</title>
             </head>
             <body>
